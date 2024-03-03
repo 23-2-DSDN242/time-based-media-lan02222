a0d1db7ca2ab18c016f06020127be52e9f64d59e
lan02222 <161381436+lan02222@users.noreply.github.com>
2024-03-04 11:20:07 +1300

    Update maeda_clock.js

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,13 +1,34 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
+  
   background(200); //  beige
-
+  angleMode(DEGREES);
   fill(0); // dark grey
-  textSize(40);
+  textSize(400);
   textAlign(CENTER, CENTER);
+  
+  translate(width/2,height/2);
+  push()
+  rotate((360/12)*11)
+  text("11", 0,0);
+  pop()
 
-  text("11", width/2, height/2);
+ let blockSize =50;
+
+  noStroke()
+  push()
+  fill(255,0,0)
+  rotate((360/12)*obj.seconds);
+  rect(0,0,blockSize,blockSize)
+  rect(0,50,blockSize,blockSize)
+  rect(0,100,blockSize,blockSize)
+  rect(50,100,blockSize,blockSize)
+  rect(100,100,blockSize,blockSize)
+  
+  pop()
+
+
 
 
   // angleMode(DEGREES);
