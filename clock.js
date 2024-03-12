5784cf1f062b9c660ed09ced6d885513cc7c7837
lan02222 <161381436+lan02222@users.noreply.github.com>
2024-03-12 15:03:21 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,6 +2,21 @@
  * use p5.js to draw a clock on a 960x500 canvas
  */
 function draw_clock(obj) {
+  background(255, 204, 0); //  morning time orange
+  fill(200)
+  noStroke()
+  rect(0, 400, width, height/2); //ground
+
+  fill(255,0,0)
+  ellipse(30, 30, 60, 60);
+
+}
+
+  
+  
+  
+  
+  
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
   //    obj.minutes goes from 0-59
@@ -27,74 +42,76 @@ function draw_clock(obj) {
   // fill(175, 133, 255); // purple
   // ellipse(width / 3 * 2, 350, 150);
 
-  noStroke()
-  angleMode(DEGREES)
-  ellipseMode (CENTER)
-  translate(width/2,height/2)
-  background(20); //  beige
-  fill(200);
+// -------------------------------------------- week 2 (4)
+//   noStroke()
+//   angleMode(DEGREES)
+//   ellipseMode (CENTER)
+//   translate(width/2,height/2)
+//   background(20); //  beige
+//   fill(200);
 
-  let OpacityAm = 35;
-  // fill(249,140,255, OpacityAm);
+//   let OpacityAm = 35;
+//   // fill(249,140,255, OpacityAm);
 
-  let blue = color(140, 255, 251,OpacityAm)
-  let purple = color(175, 133, 255,OpacityAm)
+//   let blue = color(140, 255, 251,OpacityAm)
+//   let purple = color(175, 133, 255,OpacityAm)
 
   
-  let MinForLerp = map(obj.minutes, 0 , 59, 0,1)
-  let color0f0rb = lerpColor(blue,purple,MinForLerp)
+//   let MinForLerp = map(obj.minutes, 0 , 59, 0,1)
+//   let color0f0rb = lerpColor(blue,purple,MinForLerp)
 
-  let sizeStep = 15;
-  let howManyCircles = map (obj.seconds,0, 59, 20,40) //30
+//   let sizeStep = 15;
+//   let howManyCircles = map (obj.seconds,0, 59, 20,40) //30
 
-  //console.log(howManyCircles);
+//   //console.log(howManyCircles);
 
-  fill(color0f0rb);
+//   fill(color0f0rb);
 
-  for(let i = 0; i < howManyCircles; i++){
-  ellipse(0, 0,sizeStep*i);
-  }
+//   for(let i = 0; i < howManyCircles; i++){
+//   ellipse(0, 0,sizeStep*i);
+//   }
 
 
-  fill("#Fae")
-  ellipse(0,0, 50);
+//   fill("#Fae")
+//   ellipse(0,0, 50);
 
-  let hourHand = map (obj.hours, 0, 23, 0, 360)
+//   let hourHand = map (obj.hours, 0, 23, 0, 360)
 
-push()
-for(let i = 0; i < 24; i++ ){
-  if( i == obj.hours){   
-    size = 40;
-  }
-  else{
-    size = 20
-  }
+// push()
+// for(let i = 0; i < 24; i++ ){
+//   if( i == obj.hours){   
+//     size = 40;
+//   }
+//   else{
+//     size = 20
+//   }
 
-  rotate(360 / 24)
+//   rotate(360 / 24)
 
-//   ellipse(0,-100,50)
+// //   ellipse(0,-100,50)
 
-drawTriangles(0,-200,size)
-//   fill(0)
-//   ellipse(0,115,20)
+// drawTriangles(0,-200,size)
 
-pop()
-}
+// //   fill(0)
+// //   ellipse(0,115,20)
 
-}
+// pop()
+// }
+
+// }
 
  
 
-function drawTriangles(x,y,s){
-fill(255)
-  triangle(x,y,
-            x+s/2,y+s,
-            x-s/2,y+s)
+// function drawTriangles(x,y,s){
+// fill(255)
+//   triangle(x,y,
+//             x+s/2,y+s,
+//             x-s/2,y+s)
 
-            triangle(x,y,
-               x+s/2,y-s,
-               x-s/2,y-s)
-   }
+//             triangle(x,y,
+//                x+s/2,y-s,
+//                x-s/2,y-s)
+//    }
 
 
 
